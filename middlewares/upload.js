a0c46e4d2c1dea952492328ddc48a8a35364b8a7
SMITA KADAM <smitakadam2004@gmail.com>
2026-02-01 14:59:53 +0530

    update upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,7 @@
-const multer = require("multer");
+
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
 
 
 const storage = new CloudinaryStorage({
